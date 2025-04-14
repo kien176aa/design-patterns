7cfe5ba1c4801f6d10a2be95cbf7a83876b869ee
kien <ngkien112@gmail.com>
2025-04-14 21:04:50 +0700

    event-driven cqrs

---
+++ event-driven-cqrs/src/test/java/com/dp/eventdrivencqrs/EventDrivenCqrsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dp.eventdrivencqrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventDrivenCqrsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
