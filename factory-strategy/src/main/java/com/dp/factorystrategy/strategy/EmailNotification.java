ff9d7a80f098ac4689c62f21b8a02396cd764e54
kien <ngkien112@gmail.com>
2025-05-24 21:45:59 +0700

    cm1

--- factory-strategy/src/main/java/com/dp/factorystrategy/strategy/EmailNotification.java
+++ factory-strategy/src/main/java/com/dp/factorystrategy/strategy/EmailNotification.java
@@ -7,6 +7,10 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class EmailNotification implements NotificationStrategy {
     public void send(String to, String message) {
+        String abc = "commit 1";
+        if(abc.equals("commit 2")) {
+            log.info("This is a  22 test commit");
+        }
         log.info("📧 Sending EMAIL to {}: {}", to, message);
     }
 }
